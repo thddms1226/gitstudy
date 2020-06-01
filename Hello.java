2ae62c1f2c18718399f42e3ed1c9c3ca9902b242
kiyongee2 <kiyongee2>
2020-06-01 15:25:24 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -4,10 +4,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		hello();
+		helloName("kacao");
+		helloName("Elsa");
 	}
 	
 	public static void hello() {
 		System.out.println("Hello~");
+	}
+	
+	public static void helloName(String name) {
+		System.out.println("Hello~ " + name);
 	}
 
 }
